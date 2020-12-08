f152a8aef4fde0b2bb301a89a311ea217e86aaa7
brahim024 <ifninos168@gmail.com>
2020-12-08 14:06:07 +0100

    add stor scraping

---
+++ test.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+from csv import writer
+response=requests.get('https://deals.souq.com/eg-en/cameras-and-accessories/t/9191?q=eyJzIjoic3IiLCJmIjpbXX0%3D&ref=nav')
+soup=BeautifulSoup(response.text,'html.parser')
+products=soup.findAll(class_='columns small-8 medium-12')
+with open('product.cvs','w') as csv_file:
+	csv_writer=writer(csv_file)
+	headers=['Product','Price']
+	csv_writer.writerow(headers)
+	for product in products:
+		title=product.find(class_='itemTitle').get_text().replace('\n','')
+		price=product.find(class_='is block sk-clr1')
+		print('product',product.text)
+		print('price:',price.text)
+
+		csv_writer.writerow([title,price.text])
+	
